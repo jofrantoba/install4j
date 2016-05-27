cfbebafc2e7606997b82212dfcc7647e4fdb4229
lei <lei.zhao@gridsofts.com>
2016-05-27 18:32:34 +0800

    init add

---
+++ install4j/src/main/java/com/gridsofts/install4j/core/App.java
@@ -0,0 +1,13 @@
+package com.gridsofts.install4j.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
